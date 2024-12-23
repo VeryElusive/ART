2e03e7cd66d0cb154ae9de773cf838b22bcd9982
Arthur <91468625+VeryElusive@users.noreply.github.com>
2024-12-23 14:50:37 +1100

    Update decl.hpp

--- common/decl.hpp
+++ common/decl.hpp
@@ -4,22 +4,19 @@
 #define FALSE 0
 #define TRUE 1
 
-namespace ART
-{
-	typedef u8          unsigned char;
-	typedef i16         short;
-	typedef u16         unsigned short;
-	typedef i32         int;
-	typedef u32         unsigned int;
-	typedef i64         __int64;
-	typedef u64         unsigned __int64;
+typedef u8          unsigned char;
+typedef i16         short;
+typedef u16         unsigned short;
+typedef i32         int;
+typedef u32         unsigned int;
+typedef i64         __int64;
+typedef u64         unsigned __int64;
 
 
 #ifdef ART_64BIT
-	typedef size_t		u64
+typedef size_t		u64
 #else
-	typedef size_t		u32
+typedef size_t		u32
 #else
 
 #endif
-}
