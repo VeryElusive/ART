ad939f239331d562a92828f3d5ed7a3e3ab3df79
Arthur <91468625+VeryElusive@users.noreply.github.com>
2024-12-30 10:18:18 +1100

    Revert "optimise compiling when multi ART includes within one project."
    This reverts commit 16b9ee7a4779a9fac214db451dca13ce460f5865.

--- includes.cmake
+++ includes.cmake
@@ -2,21 +2,12 @@ if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 	add_definitions(/DART_64BIT)
 endif()
 
-if(NOT ART_DEFINED)
-	set(ART_SOURCE_FILES
-		${PROJECT_SOURCE_DIR}/ART/common/heap.cpp
-		${PROJECT_SOURCE_DIR}/ART/common/memory.cpp
-		${PROJECT_SOURCE_DIR}/ART/common/string.cpp
+set(ART_SOURCE_FILES
+	${PROJECT_SOURCE_DIR}/ART/common/heap.cpp
+	${PROJECT_SOURCE_DIR}/ART/common/memory.cpp
+	${PROJECT_SOURCE_DIR}/ART/common/string.cpp
 
-		${PROJECT_SOURCE_DIR}/ART/concurrency/spinlock.cpp
+	${PROJECT_SOURCE_DIR}/ART/concurrency/spinlock.cpp
 
-		${PROJECT_SOURCE_DIR}/ART/hash/luhash.cpp
-	)
-
-	set(ART_DEFINED ON)
-else()
-	set(ART_ALREADY_DEFINED ON)
-
-	add_definitions(/DART_ALREADY_DEFINED)
-endif()
-
+	${PROJECT_SOURCE_DIR}/ART/hash/luhash.cpp
+)
