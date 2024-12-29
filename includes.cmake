16b9ee7a4779a9fac214db451dca13ce460f5865
Arthur <91468625+VeryElusive@users.noreply.github.com>
2024-12-30 09:56:31 +1100

    optimise compiling when multi ART includes within one project.

--- includes.cmake
+++ includes.cmake
@@ -2,12 +2,21 @@ if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 	add_definitions(/DART_64BIT)
 endif()
 
-set(ART_SOURCE_FILES
-	${PROJECT_SOURCE_DIR}/ART/common/heap.cpp
-	${PROJECT_SOURCE_DIR}/ART/common/memory.cpp
-	${PROJECT_SOURCE_DIR}/ART/common/string.cpp
+if(NOT ART_DEFINED)
+	set(ART_SOURCE_FILES
+		${PROJECT_SOURCE_DIR}/ART/common/heap.cpp
+		${PROJECT_SOURCE_DIR}/ART/common/memory.cpp
+		${PROJECT_SOURCE_DIR}/ART/common/string.cpp
 
-	${PROJECT_SOURCE_DIR}/ART/concurrency/spinlock.cpp
+		${PROJECT_SOURCE_DIR}/ART/concurrency/spinlock.cpp
 
-	${PROJECT_SOURCE_DIR}/ART/hash/luhash.cpp
-)
+		${PROJECT_SOURCE_DIR}/ART/hash/luhash.cpp
+	)
+
+	set(ART_DEFINED ON)
+else()
+	set(ART_ALREADY_DEFINED ON)
+
+	add_definitions(/DART_ALREADY_DEFINED)
+endif()
+
