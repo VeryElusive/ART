296f3c2e295f0f55cf820e88fc13125d06030b8a
Arthur <91468625+VeryElusive@users.noreply.github.com>
2025-01-04 12:22:38 +1100

    vector and rename color

--- datatypes/color.hpp
+++ datatypes/color.hpp
@@ -5,22 +5,22 @@
 
 namespace ART
 {
-	class Color
+	class Color_t
 	{
 	public:
-		Color(u32 color = 0x000000ff) :
+		Color_t(u32 color = 0x000000ff) :
 			r((color >> 24) & 0xFF), g((color >> 16) & 0xFF), b((color >> 8) & 0xFF), a(color & 0xFF) {}
 
-		Color(int r, int g, int b, int a = 255) :
+		Color_t(int r, int g, int b, int a = 255) :
 			r(r), g(g), b(b), a(a) {}
 
-		Color(float r, float g, float b, float a = 1.f) :
+		Color_t(float r, float g, float b, float a = 1.f) :
 			r((u8)(u32)(r * 255.f)), g((u8)(u32)(g * 255.f)), b((u8)(u32)(b * 255.f)), a((u8)(u32)(a * 255.f)) {}
 
-		Color(float Col[4]) :
+		Color_t(float Col[4]) :
 			r((u8)(u32)(Col[0] * 255.f)), g((u8)(u32)(Col[1] * 255.f)), b((u8)(u32)(Col[2] * 255.f)), a((u8)(u32)(Col[3] * 255.f)) {}
 
-		Color(u8 r, u8 g, u8 b, u8 a = 255) :
+		Color_t(u8 r, u8 g, u8 b, u8 a = 255) :
 			r(r), g(g), b(b), a(a) {}
 
 
@@ -29,14 +29,14 @@ namespace ART
 			return ((u32)(r) << 24) | ((u32)(g) << 16) | ((u32)(b) << 8) | (u32)(a);
 		}
 
-		Color ScaleAlpha(float Alpha) const
+		Color_t ScaleAlpha(float Alpha) const
 		{
 			Color ret = *this;
 			ret.a = (u8)(((float)ret.a) * Alpha);
 			return ret;
 		}
 
-		Color Lerp(const Color &To, float t) const
+		Color_t Lerp(const Color &To, float t) const
 		{
 			if(r == To.r && g == To.g && b == To.b && a == To.a)
 			{
@@ -45,7 +45,7 @@ namespace ART
 
 			t = ART::Min(t, 1.f);
 
-			Color ret(
+			Color_t ret(
 				(u8)ART::Clamp(ART::Lerp(t, (float)r, (float)To.r), 0.f, 255.f),
 				(u8)ART::Clamp(ART::Lerp(t, (float)g, (float)To.g), 0.f, 255.f),
 				(u8)ART::Clamp(ART::Lerp(t, (float)b, (float)To.b), 0.f, 255.f),
@@ -54,7 +54,7 @@ namespace ART
 			return ret;
 		}
 
-		Color &operator=(u32 color)
+		Color_t &operator=(u32 color)
 		{
 			r = (color >> 24) & 0xFF;
 			g = (color >> 16) & 0xFF;
@@ -63,7 +63,7 @@ namespace ART
 			return *this;
 		}
 
-		Color &operator=(Color color)
+		Color_t &operator=(Color_t color)
 		{
 			r = color.r;
 			g = color.g;
@@ -72,7 +72,7 @@ namespace ART
 			return *this;
 		}
 
-		bool operator==(Color color)
+		bool operator==(Color_t color)
 		{
 			return color.r == this->r
 				&& color.g == this->g
@@ -80,7 +80,7 @@ namespace ART
 				&& color.a == this->a;
 		}
 
-		bool operator!=(Color color)
+		bool operator!=(Color_t color)
 		{
 			return color.r != this->r
 				|| color.g != this->g
---
+++ datatypes/vector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../common/decl.hpp"
+#include "../common/math.hpp"
+
+namespace ART
+{
+	class Vec2_t
+	{
+	public:
+		float X, Y;
+	};
+}
