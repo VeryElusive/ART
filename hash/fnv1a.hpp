bc8ee4115a6bb692dae0142257c3b878db54e016
Arthur <91468625+VeryElusive@users.noreply.github.com>
2026-03-04 14:55:15 +1100

    Update fnv1a.hpp

--- hash/fnv1a.hpp
+++ hash/fnv1a.hpp
@@ -10,30 +10,30 @@ namespace ART
 #define BASIS 0xCBF29CE484221325ULL
 #define PRIME 0x100000001A3ULL
 
-		consteval u64 HashConst(const char *String, const u64 Key = BASIS)
+		consteval u64 HashConst(const char *String, const u64 Key = BASIS, const u64 Prime = PRIME)
 		{
-			return (String[0] == '\0') ? Key : HashConst(&String[1], (Key ^ (u64)(String[0])) * PRIME);
+			return (String[0] == '\0') ? Key : HashConst(&String[1], (Key ^ (u64)(String[0])) * Prime);
 		}
 
-		inline u64 Hash(const char *String, u64 Key = BASIS)
+		inline u64 Hash(const char *String, u64 Key = BASIS, const u64 Prime = PRIME)
 		{
 			const Size_t Length = ART::StringLength(String);
 
 			for(Size_t i = 0U; i < Length; ++i)
 			{
 				Key ^= String[i];
-				Key *= PRIME;
+				Key *= Prime;
 			}
 
 			return Key;
 		}
 
-		inline u64 Hash(u8 *Data, Size_t Length, u64 Key = BASIS)
+		inline u64 Hash(u8 *Data, Size_t Length, u64 Key = BASIS, const u64 Prime = PRIME)
 		{
 			for(Size_t i = 0U; i < Length; ++i)
 			{
 				Key ^= Data[i];
-				Key *= PRIME;
+				Key *= Prime;
 			}
 
 			return Key;
