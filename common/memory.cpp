861c2d4ce1aedd9222ed8e438d34e462c04ba833
Arthur <91468625+VeryElusive@users.noreply.github.com>
2026-03-21 16:18:25 +1100

    avx2 and sse2 detection

--- common/memory.cpp
+++ common/memory.cpp
@@ -8,6 +8,58 @@
 
 namespace ART
 {
+	typedef enum CPU_FLAGS
+	{
+		CPU_NONE = 0,
+		CPU_SSE2 = 1 << 0,
+		CPU_AVX2 = 1 << 1
+
+	} CPU_FLAGS;
+
+	inline CPU_FLAGS CPUFlags = CPU_NONE;
+	inline bool CPUFLagsHasBeenChecked = FALSE;
+
+	static void Cpuid(int out[4], int leaf)
+	{
+		__cpuid(out, leaf);
+	}
+
+	static void CpuidEx(int out[4], int leaf, int subleaf)
+	{
+		__cpuidex(out, leaf, subleaf);
+	}
+
+	CPU_FLAGS GetCpuFlags()
+	{
+		int CpuInfo[4];
+		CPU_FLAGS Flags = CPU_NONE;
+
+		Cpuid(CpuInfo, 1);
+
+		if (CpuInfo[3] & (1 << 26))
+			Flags = (CPU_FLAGS)(Flags | CPU_SSE2);
+
+		int HasAVX     = (CpuInfo[2] & (1 << 28)) != 0;
+		int HasOSXSAVE = (CpuInfo[2] & (1 << 27)) != 0;
+
+		if (HasAVX && HasOSXSAVE)
+		{
+			u64 Xcr0 = _xgetbv(0);
+
+			if ((Xcr0 & 0x6) == 0x6)
+			{
+				CpuidEx(CpuInfo, 7, 0);
+
+				if (CpuInfo[1] & (1 << 5))
+				{
+					Flags = (CPU_FLAGS)(Flags | CPU_AVX2);
+				}
+			}
+		}
+
+		return Flags;
+	}
+
 	/// <summary>
 	/// copies memory from one address to another
 	/// </summary>
@@ -16,53 +68,62 @@ namespace ART
 	/// <param name="Size">size in bytes</param>
 	void Memcpy(void *Dest, const void *Src, size_t Size)
 	{
-		u8 *d = (u8 *)(Dest);
-		const u8 *s = (const u8 *)(Src);
-
-		while(Size >= 32)
+		if(CPUFLagsHasBeenChecked == FALSE)
 		{
-			__m256i data = _mm256_loadu_si256((const __m256i *)(s));
-			_mm256_storeu_si256((__m256i *)(d), data);
-
-			d += 32;
-			s += 32;
-			Size -= 32;
+			CPUFLagsHasBeenChecked = TRUE;
+			CPUFlags = GetCpuFlags();
 		}
 
-		while(Size >= 8)
-		{
-			*(u64 *)(d) = *(u64 *)(s);
+		u8 *d = (u8 *)Dest;
+		const u8 *s = (const u8 *)Src;
 
-			s += 8;
-			d += 8;
-			Size -= 8;
+		if(Size == 0 || d == s) return;
+
+		if(CPUFlags & CPU_AVX2)
+		{
+			while(Size >= 32)
+			{
+				__m256i v = _mm256_loadu_si256((const __m256i *)s);
+				_mm256_storeu_si256((__m256i *)d, v);
+				d += 32;
+				s += 32;
+				Size -= 32;
+			}
 		}
 
-		while(Size >= 4)
+		if(CPUFlags & CPU_SSE2)
 		{
-			*(u32 *)(d) = *(u32 *)(s);
-
-			s += 4;
-			d += 4;
-			Size -= 4;
+			while(Size >= 16)
+			{
+				__m128i v = _mm_loadu_si128((const __m128i *)s);
+				_mm_storeu_si128((__m128i *)d, v);
+				d += 16;
+				s += 16;
+				Size -= 16;
+			}
 		}
 
-		while(Size >= 2)
+		if(Size >= 8)
 		{
-			*(u16 *)(d) = *(u16 *)(s);
-
-			s += 2;
-			d += 2;
-			Size -= 2;
+			*(u64 *)d = *(u64 *)s;
+			d += 8; s += 8; Size -= 8;
 		}
 
-		while(Size >= 1)
+		if(Size >= 4)
 		{
-			*(u8 *)(d) = *(u8 *)(s);
+			*(u32 *)d = *(u32 *)s;
+			d += 4; s += 4; Size -= 4;
+		}
 
-			s += 1;
-			d += 1;
-			Size -= 1;
+		if(Size >= 2)
+		{
+			*(u16 *)d = *(u16 *)s;
+			d += 2; s += 2; Size -= 2;
+		}
+
+		if(Size >= 1)
+		{
+			*d = *s;
 		}
 	}
 
@@ -74,68 +135,67 @@ namespace ART
 	/// <param name="Size">size in bytes</param>
 	void Memmove(void *Dest, const void *Src, size_t Size)
 	{
+		if(CPUFLagsHasBeenChecked == FALSE)
+		{
+			CPUFLagsHasBeenChecked = TRUE;
+			CPUFlags = GetCpuFlags();
+		}
+
 		u8 *d = (u8 *)Dest;
 		const u8 *s = (const u8 *)Src;
 
-		if(d < s || d >= s + Size)
+		if(d == s || Size == 0) return;
+
+		if(d < s)
 		{
-			Memcpy(Dest, Src, Size);
+			if(CPUFlags & CPU_AVX2)
+			{
+				while(Size >= 32)
+				{
+					_mm256_storeu_si256((__m256i *)d, _mm256_loadu_si256((const __m256i *)s));
+					d += 32; s += 32; Size -= 32;
+				}
+			}
+			if(CPUFlags & CPU_SSE2)
+			{
+				while(Size >= 16)
+				{
+					_mm_storeu_si128((__m128i *)d, _mm_loadu_si128((const __m128i *)s));
+					d += 16; s += 16; Size -= 16;
+				}
+			}
+			if(Size >= 8) { *(u64 *)d = *(u64 *)s; d += 8; s += 8; Size -= 8; }
+			if(Size >= 4) { *(u32 *)d = *(u32 *)s; d += 4; s += 4; Size -= 4; }
+			if(Size >= 2) { *(u16 *)d = *(u16 *)s; d += 2; s += 2; Size -= 2; }
+			if(Size >= 1) { *d = *s; }
 		}
 		else
 		{
 			d += Size;
 			s += Size;
 
-			while(Size >= 32)
+			if(CPUFlags & CPU_AVX2)
 			{
-				s -= 32;
-				d -= 32;
-
-				__m256i data = _mm256_loadu_si256((const __m256i *)s);
-				_mm256_storeu_si256((__m256i *)d, data);
-
-				Size -= 32;
+				while(Size >= 32)
+				{
+					d -= 32; s -= 32; Size -= 32;
+					_mm256_storeu_si256((__m256i *)d, _mm256_loadu_si256((const __m256i *)s));
+				}
 			}
-
-			while(Size >= 8)
+			if(CPUFlags & CPU_SSE2)
 			{
-				s -= 8;
-				d -= 8;
-
-				*(u64 *)d = *(const u64 *)s;
-				Size -= 8;
+				while(Size >= 16)
+				{
+					d -= 16; s -= 16; Size -= 16;
+					_mm_storeu_si128((__m128i *)d, _mm_loadu_si128((const __m128i *)s));
+				}
 			}
-
-			while(Size >= 4)
-			{
-				s -= 4;
-				d -= 4;
-
-				*(u32 *)d = *(const u32 *)s;
-				Size -= 4;
-			}
-
-			while(Size >= 2)
-			{
-				s -= 2;
-				d -= 2;
-
-				*(u16 *)d = *(const u16 *)s;
-				Size -= 2;
-			}
-
-			while(Size >= 1)
-			{
-				s -= 1;
-				d -= 1;
-
-				*(u8 *)d = *(const u8 *)s;
-				Size -= 1;
-			}
+			if(Size >= 8) { d -= 8; s -= 8; Size -= 8; *(u64 *)d = *(u64 *)s; }
+			if(Size >= 4) { d -= 4; s -= 4; Size -= 4; *(u32 *)d = *(u32 *)s; }
+			if(Size >= 2) { d -= 2; s -= 2; Size -= 2; *(u16 *)d = *(u16 *)s; }
+			if(Size >= 1) { d -= 1; s -= 1; *d = *s; }
 		}
 	}
-
-
 
 	/// <summary>
 	/// sets all bytes in a selected memory region to a particular value.
@@ -145,26 +205,74 @@ namespace ART
 	/// <param name="Size">Size in bytes</param>
 	void Memset(void *Dest, u8 Value, size_t Size)
 	{
+		if(CPUFLagsHasBeenChecked == FALSE)
+		{
+			CPUFLagsHasBeenChecked = TRUE;
+			CPUFlags = GetCpuFlags();
+		}
+
 		u8 *d = (u8 *)(Dest);
 
-		__m256i Fill32 = _mm256_set1_epi8((char)(Value));
-
-		while(Size >= 32)
+		if(CPUFlags & CPU_AVX2)
 		{
-			_mm256_storeu_si256((__m256i *)(d), Fill32);
-
-			d += 32;
-			Size -= 32;
+			__m256i Fill32 = _mm256_set1_epi8((char)Value);
+			while(Size >= 32)
+			{
+				_mm256_storeu_si256((__m256i *)d, Fill32);
+				d += 32;
+				Size -= 32;
+			}
 		}
 
-		while(Size >= 1)
+		if(CPUFlags & CPU_SSE2)
 		{
-			*(volatile u8 *)(d) = Value;
-
-			d += 1;
-			Size -= 1;
+			if(Size >= 16)
+			{
+				__m128i Fill16 = _mm_set1_epi8((char)Value);
+				while(Size >= 16)
+				{
+					_mm_storeu_si128((__m128i *)d, Fill16);
+					d += 16;
+					Size -= 16;
+				}
+			}
 		}
 
+		if(Size >= 8)
+		{
+			u64 Fill64 = 0x0101010101010101ULL * Value;
+			*(u64 *)d = Fill64;
+			d += 8;
+			Size -= 8;
+		}
+
+		if(Size >= 4)
+		{
+			u32 Fill32Scalar = 0x01010101U * Value;
+			*(u32 *)d = Fill32Scalar;
+			d += 4;
+			Size -= 4;
+		}
+
+		if(Size >= 2)
+		{
+			u16 Fill16Scalar = (u16)((Value << 8) | Value);
+			*(u16 *)d = Fill16Scalar;
+			d += 2;
+			Size -= 2;
+		}
+
+		if(Size >= 1)
+		{
+			*d = Value;
+		}
+	}
+
+	static inline u32 GetFirstMismatchIndex(u32 Mask) 
+	{
+		unsigned long Index;
+		_BitScanForward(&Index, ~Mask);
+		return (u32)Index;
 	}
 
 	/// <summary>
@@ -176,86 +284,80 @@ namespace ART
 	/// <returns>0 if the buffers are equal, -1 if the first mismatch byte in Buf1 is less than Buf2, or 1 if the first mismatch byte in Buf1 is greater than Buf2</returns>
 	i32 Memcmp(const void *Buf1, const void *Buf2, size_t Size)
 	{
-		const u8 *b1 = (const u8 *)Buf1;
-		const u8 *b2 = (const u8 *)Buf2;
-
-		while(Size >= 32)
+		if(CPUFLagsHasBeenChecked == FALSE)
 		{
-			__m256i data1 = _mm256_loadu_si256((const __m256i *)b1);
-			__m256i data2 = _mm256_loadu_si256((const __m256i *)b2);
+			CPUFLagsHasBeenChecked = TRUE;
+			CPUFlags = GetCpuFlags();
+		}
+		
+		const u8 *p1 = (const u8 *)Buf1;
+		const u8 *p2 = (const u8 *)Buf2;
 
-			if(!_mm256_testc_si256(data1, data2)) // Compare 256-bit chunks
+		if(CPUFlags & CPU_AVX2)
+		{
+			while(Size >= 32)
 			{
-				for(size_t i = 0; i < 32; ++i)
-				{
-					if(b1[i] != b2[i])
-						return (b1[i] < b2[i]) ? -1 : 1;
-				}
-			}
+				__m256i v1 = _mm256_loadu_si256((const __m256i *)p1);
+				__m256i v2 = _mm256_loadu_si256((const __m256i *)p2);
+				u32 mask = (u32)_mm256_movemask_epi8(_mm256_cmpeq_epi8(v1, v2));
 
-			b1 += 32;
-			b2 += 32;
-			Size -= 32;
+				if(mask != 0xFFFFFFFF)
+				{
+					u32 idx = GetFirstMismatchIndex(mask);
+					return (p1[idx] < p2[idx]) ? -1 : 1;
+				}
+				p1 += 32; p2 += 32; Size -= 32;
+			}
 		}
 
-		while(Size >= 8)
+		if(CPUFlags & CPU_SSE2)
 		{
-			if(*(u64 *)b1 != *(u64 *)b2)
+			while(Size >= 16)
 			{
-				for(size_t i = 0; i < 8; ++i)
+				__m128i v1 = _mm_loadu_si128((const __m128i *)p1);
+				__m128i v2 = _mm_loadu_si128((const __m128i *)p2);
+				u32 mask = (u32)_mm_movemask_epi8(_mm_cmpeq_epi8(v1, v2)) & 0xFFFF;
+
+				if(mask != 0xFFFF)
 				{
-					if(b1[i] != b2[i])
-						return (b1[i] < b2[i]) ? -1 : 1;
+					u32 idx = GetFirstMismatchIndex(mask);
+					return (p1[idx] < p2[idx]) ? -1 : 1;
 				}
+				p1 += 16; p2 += 16; Size -= 16;
 			}
-
-			b1 += 8;
-			b2 += 8;
-			Size -= 8;
 		}
 
-		while(Size >= 4)
+		if(Size >= 8)
 		{
-			if(*(u32 *)b1 != *(u32 *)b2)
-			{
-				for(size_t i = 0; i < 4; ++i)
-				{
-					if(b1[i] != b2[i])
-						return (b1[i] < b2[i]) ? -1 : 1;
-				}
-			}
-
-			b1 += 4;
-			b2 += 4;
-			Size -= 4;
+			if(*(u64 *)p1 != *(u64 *)p2) goto tail_mismatch;
+			p1 += 8; p2 += 8; Size -= 8;
 		}
 
-		while(Size >= 2)
+		if(Size >= 4)
 		{
-			if(*(u16 *)b1 != *(u16 *)b2)
-			{
-				for(size_t i = 0; i < 2; ++i)
-				{
-					if(b1[i] != b2[i])
-						return (b1[i] < b2[i]) ? -1 : 1;
-				}
-			}
-
-			b1 += 2;
-			b2 += 2;
-			Size -= 2;
+			if(*(u32 *)p1 != *(u32 *)p2) goto tail_mismatch;
+			p1 += 4; p2 += 4; Size -= 4;
 		}
 
-		while(Size >= 1)
+		if(Size >= 2)
 		{
-			if(*b1 != *b2)
-				return (*b1 < *b2) ? -1 : 1;
-
-			b1 += 1;
-			b2 += 1;
-			Size -= 1;
+			if(*(u16 *)p1 != *(u16 *)p2) goto tail_mismatch;
+			p1 += 2; p2 += 2; Size -= 2;
 		}
 
-		return 0; // Buffers are equal
+		if(Size >= 1)
+		{
+			if(*p1 != *p2) goto tail_mismatch;
+		}
+
+		return 0;
+
+	tail_mismatch:
+		while(Size > 0)
+		{
+			if(*p1 != *p2) return (*p1 < *p2) ? -1 : 1;
+			p1++; p2++; Size--;
+		}
+		return 0;
 	}
 }
