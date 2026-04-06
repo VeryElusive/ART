020943b4581911ff25e660219e2822f98e76b585
Arthur <91468625+VeryElusive@users.noreply.github.com>
2026-04-06 15:12:09 +1000

    fix sort compiler error

--- sorting/sort.hpp
+++ sorting/sort.hpp
@@ -1,9 +1,43 @@
+// sort.hpp
 #pragma once
-
 #include "../common/decl.hpp"
 
 namespace ART
 {
-	template <typename T = Size_t>
-	void QuickSort(T Arr[], Size_t Size);
+	namespace Detail
+	{
+		template<typename T>
+		void Swap(T &a, T &b)
+		{
+			T tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		template<typename T>
+		Size_t Partition(T Arr[], Size_t Size)
+		{
+			T pivot = Arr[Size - 1];
+			int i = -1;
+			for(int j = 0; j < Size - 1; j++)
+			{
+				if(Arr[j] <= pivot)
+				{
+					i++;
+					Detail::Swap(Arr[i], Arr[j]);
+				}
+			}
+			Detail::Swap(Arr[i + 1], Arr[Size - 1]);
+			return (i + 1);
+		}
+	}
+
+	template<typename T>
+	void QuickSort(T Arr[], Size_t Size)
+	{
+		if(Size <= 1) return;
+		Size_t pi = Detail::Partition(Arr, Size);
+		QuickSort(Arr, pi);
+		QuickSort(Arr + pi + 1, Size - pi - 1);
+	}
 }
